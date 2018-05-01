5356e32d9cb4788477157d0b9d9d86224fc619de
hezhihao <gaoyuanhezhihao@139.com>
2018-05-01 21:01:39 +0800

    solver: + image to global, global to image pose solver

--- solver/pose.cpp
+++ solver/pose.cpp
@@ -8,6 +8,7 @@
 
 #include "pose.hpp"
 #include "core.hpp"
+#include "config/Config.hpp"
 
 using namespace std;
 using ceres::AutoDiffCostFunction;
@@ -40,6 +41,97 @@ struct Affine2D {
     double cur[2];
     double prev[2];
 };
+
+struct Global2Img{
+    double _g_pt[2];
+    double _im_pt[2];
+    Global2Img(const double im_pt[2], const double global_pt[2]){
+        _g_pt[0] = global_pt[0];
+        _g_pt[1] = global_pt[1];
+        _im_pt[0] = im_pt[0];
+        _im_pt[1] = im_pt[1];
+    }
+
+    template<typename T> 
+        bool operator()(const T*theta, const T* t, T * residuals) const {
+            const T q_0 = cos(theta[0]) * _g_pt[0] + sin(theta[0]) * _g_pt[1] - t[0];
+            const T q_1 = -sin(theta[0]) * _g_pt[0] + cos(theta[0]) * _g_pt[1] - t[1];
+            residuals[0] = q_0 - _im_pt[0];
+            residuals[1] = q_1 - _im_pt[1];
+            return true;
+        }
+};
+
+struct Img2Global{
+    double _g_pt[2];
+    double _im_pt[2];
+    Img2Global(const double im_pt[2], const double global_pt[2]){
+        _g_pt[0] = global_pt[0];
+        _g_pt[1] = global_pt[1];
+        _im_pt[0] = im_pt[0];
+        _im_pt[1] = im_pt[1];
+
+    }
+
+    template<typename T> 
+        bool operator()(const T*theta, const T* t, T * residuals) const {
+            const T q_0 = cos(theta[0]) * _im_pt[0] - sin(theta[0]) * _im_pt[1] + t[0];
+            const T q_1 = sin(theta[0]) * _im_pt[0] + cos(theta[0]) * _im_pt[1] + t[1];
+            residuals[0] = q_0 - _g_pt[0];
+            residuals[1] = q_1 - _g_pt[1];
+            return true;
+        }
+};
+
+bool solve_2D_im2g(const vector<array<double, 2>> & im_pts, const vector<array<double, 2>> & global_pts, double & _theta, double _t[2]) {
+    static const double max_cost = get_param("ceres_error_thres");
+    Problem problem;
+    const int cnt = global_pts.size();
+    assert(cnt >= 2);
+
+
+    //double _theta=0;
+    //double _t[2]={0., 0.};
+    cout << "DEBUG solve_2D_im2g ---" << endl;
+    for(int i = 0; i < cnt; ++i){
+        ceres::CostFunction * cost_func = new AutoDiffCostFunction<Img2Global, 2, 1, 2> (new Img2Global(im_pts[i].data(), global_pts[i].data()));
+        cout <<  im_pts[i].data()[0] << ", " << im_pts[i].data()[1] << "--" << 
+            global_pts[i].data()[0] << ", "  << global_pts[i].data()[1] << endl;
+        problem.AddResidualBlock(cost_func, NULL, &_theta, _t);
+    }
+    cout << "DEBUG solve_2D_im2g ***"<< endl;
+    Solver::Options opt;
+    opt.max_num_iterations = 1000;
+    opt.linear_solver_type = ceres::DENSE_QR;
+    opt.minimizer_progress_to_stdout = false;
+    Solver::Summary summary;
+    Solve(opt, &problem, &summary);
+    cout << "sovler final cost=" << summary.final_cost << endl;
+    //assert(summary.final_cost/cnt < 10);
+    //std::cout << summary.BriefReport() << "\n";
+    return (summary.final_cost/cnt) < max_cost;
+}
+
+bool solve_2D_g2im(const vector<array<double, 2>> & cur_pts, const vector<array<double, 2>> & prev_pts, double & _theta, double _t[2]) {
+    Problem problem;
+    const int cnt = prev_pts.size();
+
+
+    //double _theta=0;
+    //double _t[2]={0., 0.};
+    for(int i = 0; i < cnt; ++i){
+        ceres::CostFunction * cost_func = new AutoDiffCostFunction<Global2Img, 2, 1, 2> (new Global2Img(cur_pts[i].data(), prev_pts[i].data()));
+        problem.AddResidualBlock(cost_func, NULL, &_theta, _t);
+    }
+    Solver::Options opt;
+    opt.max_num_iterations = 100;
+    opt.linear_solver_type = ceres::DENSE_QR;
+    opt.minimizer_progress_to_stdout = false;
+    Solver::Summary summary;
+    Solve(opt, &problem, &summary);
+    //std::cout << summary.BriefReport() << "\n";
+    return true;
+}
 
 bool solve_2D_pose(const vector<array<double, 2>> & cur_pts, const vector<array<double, 2>> & prev_pts, double & _theta, double _t[2]) {
     Problem problem;
--- solver/pose.hpp
+++ solver/pose.hpp
@@ -15,4 +15,6 @@ class Pose2D{
 };
 
 bool solve_2D_pose(const vector<array<double, 2>> & cur_pts, const vector<array<double, 2>> & prev_pts, double & _theta, double _t[2]);
+bool solve_2D_g2im(const vector<array<double, 2>> & cur_pts, const vector<array<double, 2>> & prev_pts, double & _theta, double _t[2]);
+bool solve_2D_im2g(const vector<array<double, 2>> & im_pts, const vector<array<double, 2>> & global_pts, double & _theta, double _t[2]);
 #endif //POSE_H
